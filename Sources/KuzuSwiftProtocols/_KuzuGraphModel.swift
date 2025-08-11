facc5f91a45d237e20a1c283c1d2cfd387bdfd5e
1amageek <tmy0x3@icloud.com>
2025-08-11 15:25:47 +0900

    Add Sendable conformance to _KuzuGraphModel protocol

--- Sources/KuzuSwiftProtocols/_KuzuGraphModel.swift
+++ Sources/KuzuSwiftProtocols/_KuzuGraphModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-public protocol _KuzuGraphModel {
+// Swift 6: Metatypes of Sendable types are automatically Sendable
+public protocol _KuzuGraphModel: Sendable, SendableMetatype {
     static var _kuzuDDL: String { get }
     static var _kuzuColumns: [(name: String, type: String, constraints: [String])] { get }
 }
