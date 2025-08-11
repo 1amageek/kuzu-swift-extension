cedd319655addbc0a5a1e5b6411a36e9dc6a6f15
1amageek <tmy0x3@icloud.com>
2025-08-11 18:11:57 +0900

    update Swift tools version to 6.2 and modify protocol

--- Sources/KuzuSwiftProtocols/_KuzuGraphModel.swift
+++ Sources/KuzuSwiftProtocols/_KuzuGraphModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // Swift 6: Metatypes of Sendable types are automatically Sendable
-public protocol _KuzuGraphModel: Sendable, SendableMetatype {
+public protocol _KuzuGraphModel: Sendable {
     static var _kuzuDDL: String { get }
     static var _kuzuColumns: [(name: String, type: String, constraints: [String])] { get }
 }
